6f90aa4dbbdb5731bfb72d158efd23a37e7756d4
arangoarangojoansebastian0-afk <arangoarangojoansebastian0@gmail.com>
2026-08-26 14:32:36 -0500

    feat: dynamic CMS pages and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import TeamMember from "./pages/TeamMember";
 import NotFound from "./pages/NotFound";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
-import content from "./data/content.json";
+
 
 function Home() {
   return <Hero />;
